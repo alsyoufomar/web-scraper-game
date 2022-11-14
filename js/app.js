42d5d05eab61af6603404d28975421854e9d7130
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-11-14 22:05:57 +0000

    add responsiveness

--- js/app.js
+++ js/app.js
@@ -1,299 +1,299 @@
 const theGame = () => {
-  const board = document.querySelector('.board');
-  const bookmarksLeft = document.querySelector('.bookmarksLeft');
-  const bookmarksUsed = document.querySelector('.bookmarksUsed');
-  const facebooksNum = document.querySelector('.facebooksNum');
-  const result = document.querySelector('.result');
-  const thugLifetime = document.querySelector('.thugLife');
-  const lobby = document.querySelector('.lobby');
-  let classy = 'easy';
-  let widthy = 10;
-  let numy = 10;
-  leveling();
-  document.body.setAttribute('class', classy);
-  const boardWidth = widthy;
-  const allCells = [];
-  const minesNum = numy;
-  let isGameOver = false;
-  const boardArea = boardWidth ** 2;
-  let bookmarks = 0;
-  const levelTitle = document.querySelector('.level__title');
+  const board = document.querySelector('.board')
+  const bookmarksLeft = document.querySelector('.bookmarksLeft')
+  const bookmarksUsed = document.querySelector('.bookmarksUsed')
+  const facebooksNum = document.querySelector('.facebooksNum')
+  const result = document.querySelector('.result')
+  const thugLifetime = document.querySelector('.thugLife')
+  const lobby = document.querySelector('.lobby')
+  let classy = 'easy'
+  let widthy = 10
+  let numy = 10
+  leveling()
+  document.body.setAttribute('class', classy)
+  const boardWidth = widthy
+  const allCells = []
+  const minesNum = numy
+  let isGameOver = false
+  const boardArea = boardWidth ** 2
+  let bookmarks = 0
+  const levelTitle = document.querySelector('.level__title')
 
-  levelTitle.innerHTML = levelup === null ? 'Silly' : levelup;
-  bookmarksLeft.innerHTML = `💾 left = ${minesNum - bookmarks}`;
-  bookmarksUsed.innerHTML = `Marked URLs = ${bookmarks}`;
-  facebooksNum.innerHTML = `FB URLs total = ${minesNum}`;
-  result.innerHTML = ' ';
+  levelTitle.innerHTML = levelup === null ? 'Silly' : levelup
+  bookmarksLeft.innerHTML = `💾 left = ${minesNum - bookmarks}`
+  bookmarksUsed.innerHTML = `Marked URLs = ${bookmarks}`
+  facebooksNum.innerHTML = `FB URLs total = ${minesNum}`
+  result.innerHTML = ' '
 
-  const audioScrape = new Audio();
-  const errorAudio = new Audio();
-  const errBG = new Audio();
-  const winnerAudio = new Audio();
-  const lobbyBtn = new Audio();
-  lobbyBtn.src = '../audio/tech.wav';
-  audioScrape.src = '../audio/select.wav';
-  errorAudio.src = '../audio/error.wav';
-  errBG.src = '../audio/fail.wav';
-  backgroundAudio.src = '../audio/bg.mp3';
-  winnerAudio.src = '../audio/winner.mp3';
+  const audioScrape = new Audio()
+  const errorAudio = new Audio()
+  const errBG = new Audio()
+  const winnerAudio = new Audio()
+  const lobbyBtn = new Audio()
+  lobbyBtn.src = '../audio/tech.wav'
+  audioScrape.src = '../audio/select.wav'
+  errorAudio.src = '../audio/error.wav'
+  errBG.src = '../audio/fail.wav'
+  backgroundAudio.src = '../audio/bg.mp3'
+  winnerAudio.src = '../audio/winner.mp3'
 
-  if (!isGameOver) backgroundAudio.play();
+  if (!isGameOver) backgroundAudio.play()
 
   lobby.addEventListener('click', (e) => {
-    lobbyBtn.play();
-    backgroundAudio.pause();
-  });
-
-  (function () {
-    const mines = Array(minesNum).fill('mine');
-    const safe = Array(boardArea - minesNum).fill('safe');
-    const shuffledArr = mines.concat(safe).sort(() => Math.random() - 0.5);
+    lobbyBtn.play()
+    backgroundAudio.pause()
+  })
+  ;(function () {
+    const mines = Array(minesNum).fill('mine')
+    const safe = Array(boardArea - minesNum).fill('safe')
+    const shuffledArr = mines.concat(safe).sort(() => Math.random() - 0.5)
 
     for (let i = 0; i < boardArea; i++) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      cell.classList.add(shuffledArr[i]);
-      cell.setAttribute('id', i);
-      board.append(cell);
-      allCells.push(cell);
+      const cell = document.createElement('div')
+      cell.classList.add('cell')
+      cell.classList.add(shuffledArr[i])
+      cell.setAttribute('id', i)
+      board.append(cell)
+      allCells.push(cell)
 
       cell.addEventListener('click', (e) => {
-        clickMe(cell);
+        clickMe(cell)
         if (!isGameOver) {
-          audioScrape.load(); //this will predownload the audio into the buffer
-          audioScrape.play();
+          audioScrape.load() //this will predownload the audio into the buffer
+          audioScrape.play()
         }
-      });
+      })
 
       cell.addEventListener('contextmenu', (e) => {
-        e.preventDefault();
-        addBookmark(cell);
-      });
+        e.preventDefault()
+        addBookmark(cell)
+      })
     }
 
     for (let i = 0; i < allCells.length; i++) {
-      let total = 0;
-      const isLeftEdge = i % boardWidth === 0;
-      const isRightEdge = i % boardWidth === boardWidth - 1;
-      const isTop = i < boardWidth;
-      const isBottom = i >= boardArea - boardWidth;
+      let total = 0
+      const isLeftEdge = i % boardWidth === 0
+      const isRightEdge = i % boardWidth === boardWidth - 1
+      const isTop = i < boardWidth
+      const isBottom = i >= boardArea - boardWidth
       if (allCells[i].classList.contains('safe')) {
         if (i > 0 && !isLeftEdge && allCells[i - 1].classList.contains('mine'))
-          total++;
-        if (!isRightEdge && allCells[i + 1].classList.contains('mine')) total++;
+          total++
+        if (!isRightEdge && allCells[i + 1].classList.contains('mine')) total++
         if (!isTop && allCells[i - boardWidth].classList.contains('mine'))
-          total++;
+          total++
         if (!isBottom && allCells[i + boardWidth].classList.contains('mine'))
-          total++;
+          total++
         if (
           i > boardWidth &&
           !isLeftEdge &&
           allCells[i - 1 - boardWidth].classList.contains('mine')
         )
-          total++;
+          total++
         if (
           i > boardWidth - 1 &&
           !isRightEdge &&
           allCells[i + 1 - boardWidth].classList.contains('mine')
         )
-          total++;
+          total++
         if (
           i < boardArea - boardWidth &&
           !isLeftEdge &&
           allCells[i - 1 + boardWidth].classList.contains('mine')
         )
-          total++;
+          total++
         if (
           !isBottom &&
           !isRightEdge &&
           allCells[i + 1 + boardWidth].classList.contains('mine')
         )
-          total++;
-        allCells[i].setAttribute('data', total);
+          total++
+        allCells[i].setAttribute('data', total)
       }
     }
-  })();
+  })()
 
   function clickMe(cell) {
-    if (isGameOver) errorAudio.play();
+    if (isGameOver) errorAudio.play()
 
-    const id = parseInt(cell.id);
+    const id = parseInt(cell.id)
     if (
       isGameOver ||
       cell.classList.contains('checked') ||
       cell.classList.contains('bookmark')
     )
-      return;
+      return
     if (cell.classList.contains('mine')) {
-      cell.classList.add('checked-mine');
-      cell.innerHTML = 'f';
-      gameOver();
+      cell.classList.add('checked-mine')
+      cell.innerHTML = 'f'
+      gameOver()
     } else {
-      let total = cell.getAttribute('data');
+      let total = cell.getAttribute('data')
       if (total != 0) {
-        cell.classList.add('checked');
-        cell.innerHTML = total;
-        return;
+        cell.classList.add('checked')
+        cell.innerHTML = total
+        return
       }
-      spread(id);
+      spread(id)
     }
-    cell.classList.add('checked');
+    cell.classList.add('checked')
   }
 
   function spread(id) {
-    const isLeftEdge = id % boardWidth === 0;
-    const isRightEdge = id % boardWidth === boardWidth - 1;
-    const isTop = id < boardWidth;
-    const isBottom = id >= boardArea - boardWidth;
+    const isLeftEdge = id % boardWidth === 0
+    const isRightEdge = id % boardWidth === boardWidth - 1
+    const isTop = id < boardWidth
+    const isBottom = id >= boardArea - boardWidth
 
     setTimeout(() => {
       if (id < boardArea - 2 && !isRightEdge) {
-        const newId = parseInt(id) + 1;
-        const newCell = document.getElementById(newId);
-        clickMe(newCell);
+        const newId = parseInt(id) + 1
+        const newCell = document.getElementById(newId)
+        clickMe(newCell)
       }
       if (id > 0 && !isLeftEdge) {
-        const newId = parseInt(id) - 1;
+        const newId = parseInt(id) - 1
 
-        const newCell = document.getElementById(newId);
-        clickMe(newCell);
+        const newCell = document.getElementById(newId)
+        clickMe(newCell)
       }
       if (!isTop) {
-        const newId = parseInt(id) - boardWidth;
-        const newCell = document.getElementById(newId);
-        clickMe(newCell);
+        const newId = parseInt(id) - boardWidth
+        const newCell = document.getElementById(newId)
+        clickMe(newCell)
       }
       if (!isBottom) {
-        const newId = parseInt(id) + boardWidth;
-        const newCell = document.getElementById(newId);
-        clickMe(newCell);
+        const newId = parseInt(id) + boardWidth
+        const newCell = document.getElementById(newId)
+        clickMe(newCell)
       }
       if (id > boardWidth - 1 && !isRightEdge) {
-        const newId = parseInt(id) + 1 - boardWidth;
-        const newCell = document.getElementById(newId);
-        clickMe(newCell);
+        const newId = parseInt(id) + 1 - boardWidth
+        const newCell = document.getElementById(newId)
+        clickMe(newCell)
       }
       if (id > boardWidth + 1 && !isLeftEdge) {
-        const newId = parseInt(id) - 1 - boardWidth;
-        const newSquare = document.getElementById(newId);
-        clickMe(newSquare);
+        const newId = parseInt(id) - 1 - boardWidth
+        const newSquare = document.getElementById(newId)
+        clickMe(newSquare)
       }
       if (id < boardArea - boardWidth - 2 && !isRightEdge) {
-        const newId = parseInt(id) + 1 + boardWidth;
-        const newCell = document.getElementById(newId);
-        clickMe(newCell);
+        const newId = parseInt(id) + 1 + boardWidth
+        const newCell = document.getElementById(newId)
+        clickMe(newCell)
       }
       if (id < boardArea - boardWidth && !isLeftEdge) {
-        const newId = parseInt(id) - 1 + boardWidth;
-        const newSquare = document.getElementById(newId);
-        clickMe(newSquare);
+        const newId = parseInt(id) - 1 + boardWidth
+        const newSquare = document.getElementById(newId)
+        clickMe(newSquare)
       }
-    }, 20);
+    }, 20)
   }
 
   function gameOver() {
-    result.innerHTML = '<p>We got you <br/> Mr hacker!!</p>';
-    isGameOver = true;
-    backgroundAudio.pause();
-    errBG.play();
+    result.style.display = 'flex'
+    result.innerHTML = '<p>We got you <br/> Mr hacker!!</p>'
+    isGameOver = true
+    backgroundAudio.pause()
+    errBG.play()
     for (let cell of allCells) {
       if (cell.classList.contains('mine')) {
-        cell.classList.add('checked-mine');
-        cell.innerHTML = 'f';
+        cell.classList.add('checked-mine')
+        cell.innerHTML = 'f'
       }
     }
   }
 
   function addBookmark(cell) {
-    if (isGameOver) return;
+    if (isGameOver) return
     if (!cell.classList.contains('checked')) {
       if (!cell.classList.contains('bookmark')) {
         if (bookmarks < minesNum) {
-          cell.classList.add('bookmark');
-          bookmarks++;
-          cell.innerHTML = '💾';
-          bookmarksLeft.innerHTML = `💾 left = ${minesNum - bookmarks}`;
-          bookmarksUsed.innerHTML = `Marked URLs = ${bookmarks}`;
-          checkWinner();
+          cell.classList.add('bookmark')
+          bookmarks++
+          cell.innerHTML = '💾'
+          bookmarksLeft.innerHTML = `💾 left = ${minesNum - bookmarks}`
+          bookmarksUsed.innerHTML = `Marked URLs = ${bookmarks}`
+          checkWinner()
         }
       } else {
-        console.log('activated');
-        cell.classList.remove('bookmark');
-        bookmarks--;
-        cell.innerHTML = '';
-        bookmarksLeft.innerHTML = `💾 left = ${minesNum - bookmarks}`;
-        bookmarksUsed.innerHTML = `Marked URLs = ${bookmarks}`;
+        console.log('activated')
+        cell.classList.remove('bookmark')
+        bookmarks--
+        cell.innerHTML = ''
+        bookmarksLeft.innerHTML = `💾 left = ${minesNum - bookmarks}`
+        bookmarksUsed.innerHTML = `Marked URLs = ${bookmarks}`
       }
     }
   }
 
   function checkWinner() {
-    let matches = 0;
+    let matches = 0
 
     for (let cell of allCells) {
       if (
         cell.classList.contains('mine') &&
         cell.classList.contains('bookmark')
       ) {
-        matches++;
+        matches++
       }
       if (matches === minesNum) {
-        result.innerHTML = '<p>Thug Life <br/> Certified!!</p>';
-        thugLifetime.style.display = 'block';
-        backgroundAudio.pause();
-        winnerAudio.play();
-        isGameOver = true;
+        result.innerHTML = '<p>Thug Life <br/> Certified!!</p>'
+        thugLifetime.style.display = 'block'
+        backgroundAudio.pause()
+        winnerAudio.play()
+        isGameOver = true
       }
     }
   }
 
   function leveling() {
     switch (levelup) {
-      case 'Silly':
-        widthy = 10;
-        classy = 'easy';
-        numy = 10;
-        break;
-      case 'Noob':
-        widthy = 10;
-        classy = 'easy';
-        numy = 15;
-        break;
-      case 'Zenitsu Agatsuma':
-        widthy = 10;
-        classy = 'easy';
-        numy = 20;
-        break;
-      case 'Tanjiro Kamado':
-        widthy = 15;
-        classy = 'medium';
-        numy = 23;
-        break;
-      case 'Naruto':
-        widthy = 15;
-        classy = 'medium';
-        numy = 34;
-        break;
-      case 'Super Saiyan':
-        widthy = 15;
-        classy = 'medium';
-        numy = 45;
-        break;
-      case 'Saitama':
-        widthy = 20;
-        classy = 'hard';
-        numy = 40;
-        break;
-      case 'Walter White':
-        widthy = 20;
-        classy = 'hard';
-        numy = 60;
-        break;
-      case 'Boolean Developer':
-        widthy = 20;
-        classy = 'hard';
-        numy = 80;
-        break;
+      case 'Level 1':
+        widthy = 10
+        classy = 'easy'
+        numy = 10
+        break
+      case 'Level 2':
+        widthy = 10
+        classy = 'easy'
+        numy = 15
+        break
+      case 'Level 3':
+        widthy = 10
+        classy = 'easy'
+        numy = 20
+        break
+      case 'Level 4':
+        widthy = 15
+        classy = 'medium'
+        numy = 23
+        break
+      case 'Level 5':
+        widthy = 15
+        classy = 'medium'
+        numy = 34
+        break
+      case 'Level 6':
+        widthy = 15
+        classy = 'medium'
+        numy = 45
+        break
+      case 'Level 7':
+        widthy = 20
+        classy = 'hard'
+        numy = 40
+        break
+      case 'Level 8':
+        widthy = 20
+        classy = 'hard'
+        numy = 60
+        break
+      case 'Level 9':
+        widthy = 20
+        classy = 'hard'
+        numy = 80
+        break
     }
   }
-};
+}
--- js/entry.js
+++ js/entry.js
@@ -1,78 +1,78 @@
-var levelup = null;
+var levelup = null
 function entryMessage() {
-  const intro = document.querySelector('.intro');
+  const intro = document.querySelector('.intro')
   const warning =
-    '<p>You have a grid of URLs, <strong>Left-click</strong> to scrape data and <strong>Right-click</strong> to bookmark Facebook URL so you can avoid it.</p> <p> Your mission is to scrape all websites but Facebook, otherwise, Facebook will sue you!</p> <p>Good Luck Mr Hacker!</p>';
+    '<p>You have a grid of URLs, <strong>Left-click</strong> to scrape data and <strong>Right-click</strong> to bookmark Facebook URL so you can avoid it.</p> <p> Your mission is to scrape all websites but Facebook, otherwise, Facebook will sue you!</p> <p>Good Luck Mr Hacker!</p>'
 
-  (function setTime() {
-    let time = Math.round(Math.random() * (2000 - 500)) + 500;
+  ;(function setTime() {
+    let time = Math.round(Math.random() * (2000 - 500)) + 500
     setTimeout(() => {
-      if (time < 900) intro.innerText = 'HELP_ WORLD';
-      else intro.innerText = 'HELLO WORLD';
-      setTime();
-    }, time);
-  })();
+      if (time < 900) intro.innerText = 'HELP_ WORLD'
+      else intro.innerText = 'HELLO WORLD'
+      setTime()
+    }, time)
+  })()
 
-  const audioScrape = new Audio();
-  audioScrape.src = '../audio/tech.wav';
+  const audioScrape = new Audio()
+  audioScrape.src = '../audio/tech.wav'
 
   function mession() {
-    const isDisplayed = sessionStorage.getItem('popupDisplayed');
-    const message = document.querySelector('.message');
-    const closeBtn = document.querySelector('.close_btn');
-    const messageBody = document.querySelector('.message_body');
-    message.append(closeBtn);
+    const isDisplayed = sessionStorage.getItem('popupDisplayed')
+    const message = document.querySelector('.message')
+    const closeBtn = document.querySelector('.close_btn')
+    const messageBody = document.querySelector('.message_body')
+    message.append(closeBtn)
 
     if (isDisplayed !== 'true') {
       setTimeout(() => {
-        message.classList.add('popup');
-        closeBtn.classList.add('popup_btn');
-        closeBtn.innerHTML = 'Understood';
-        messageBody.innerHTML = warning;
+        message.classList.add('popup')
+        closeBtn.classList.add('popup_btn')
+        closeBtn.innerHTML = 'Understood'
+        messageBody.innerHTML = warning
 
-        const test = document.querySelector('.notification_audio');
-        test.muted = false;
-        test.autoplay = true;
-        const promise = test.play();
+        const test = document.querySelector('.notification_audio')
+        test.muted = false
+        test.autoplay = true
+        const promise = test.play()
         if (promise !== undefined) {
           promise
             .then((_) => {
-              console.log('Autoplay started');
+              console.log('Autoplay started')
               // Autoplay started!
             })
             .catch((error) => {
-              console.log(error);
+              console.log(error)
               // Autoplay was prevented.
-            });
+            })
         }
-      }, 1500);
+      }, 1500)
 
       closeBtn.addEventListener('click', (e) => {
-        audioScrape.play();
-        sessionStorage.setItem('popupDisplayed', 'true');
-        closeBtn.innerHTML = '';
-        messageBody.innerHTML = '';
-        entryMessage();
-      });
+        audioScrape.play()
+        sessionStorage.setItem('popupDisplayed', 'true')
+        closeBtn.innerHTML = ''
+        messageBody.innerHTML = ''
+        entryMessage()
+      })
     } else {
-      closeBtn.classList.remove('popup_btn');
-      message.classList.remove('popup');
+      closeBtn.classList.remove('popup_btn')
+      message.classList.remove('popup')
     }
   }
-  mession();
+  mession()
 
-  const startBtn = document.querySelector('.start__btn');
+  const startBtn = document.querySelector('.start__btn')
   startBtn.addEventListener('click', (e) => {
-    audioScrape.play();
-  });
+    audioScrape.play()
+  })
 
-  const levelBtn = document.querySelector('.start__level');
+  const levelBtn = document.querySelector('.start__level')
   levelBtn.addEventListener('click', (e) => {
-    audioScrape.play();
-  });
+    audioScrape.play()
+  })
 
-  const levelsPick = document.querySelector('#levelsPick');
+  const levelsPick = document.querySelector('#levelsPick')
   levelsPick.addEventListener('change', (e) => {
-    levelup = e.target.value;
-  });
+    levelup = e.target.value
+  })
 }
